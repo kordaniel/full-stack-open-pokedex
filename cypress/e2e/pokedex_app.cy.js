8c063a474b8c7a03d5bfeb117e001de9453cdddb
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2023-07-11 06:21:27 +0300

    Part 11, ex: 9

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,7 +1,19 @@
 describe('Pokedex', () => {
-  it('front page can be opened', () => {
+  beforeEach(() => {
     cy.visit('')
+  })
+
+  it('front page can be opened', () => {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('ivysaur can be navigated to', () => {
+    cy.contains('ivysaur').click()
+    cy.contains('speed')
+    cy.contains('special defense')
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
+
 })
