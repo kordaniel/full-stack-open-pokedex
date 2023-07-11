c853aa444549248268dc535da18462ebd8d9a9d7
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2023-07-11 06:00:21 +0300

    Part 11, ex9-1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -22,7 +23,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
