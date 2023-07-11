be60f12f2d07136feaf93110ae5cd754232a33bf
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2023-07-11 08:23:16 +0300

    Part 11, ex: 12

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11.12.0')
+  res.send('11.12.1')
 })
 
 app.get('/health', (req, res) => {
-  throw 'error'
+  //throw 'error'
   // eslint-disable-next-line no-unreachable
   res.send('OK')
 })
