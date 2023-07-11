1ee3546c4c0f0343e122104d5582f7e990c4c730
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2023-07-11 08:17:18 +0300

    Part 11, ex 12-1

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('11.12.0')
+})
+
+app.get('/health', (req, res) => {
+  throw 'error'
+  // eslint-disable-next-line no-unreachable
+  res.send('OK')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
